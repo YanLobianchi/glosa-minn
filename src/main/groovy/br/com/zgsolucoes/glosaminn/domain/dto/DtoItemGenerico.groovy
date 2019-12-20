dc7a576dafddb34cb230fa2f9c43c55619645d2d
Paulo Vitor <paulo.silva@zgsolucoes.com.br>
2019-12-20 11:43:49 -0300

    Criação de DTO genéricos para Guia e Item

---
+++ src/main/groovy/br/com/zgsolucoes/glosaminn/domain/dto/DtoItemGenerico.groovy
@@ -0,0 +1,9 @@
+package br.com.zgsolucoes.glosaminn.domain.dto
+
+class DtoItemGenerico {
+    String nomeItem
+    String codigoItem
+    BigDecimal valorApresentado
+    BigDecimal valorGlosado
+    BigDecimal valorPago
+}
