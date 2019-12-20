6c85ffc6d20b74d31fca1a33109a6be43d91c0df
henriquegomes <henrique.gomes@zgsolucoes.com.br>
2019-12-20 11:06:47 -0300

    Commit inicial da conciliação

---
+++ src/main/groovy/br/com/zgsolucoes/glosaminn/service/conciliacao/ConciliacaoService.groovy
@@ -0,0 +1,13 @@
+package br.com.zgsolucoes.glosaminn.service.conciliacao
+
+import br.com.zgsolucoes.glosaminn.guia.Guia
+import br.com.zgsolucoes.glosaminn.guia.GuiaConvenio
+import br.com.zgsolucoes.glosaminn.guia.GuiaHospital
+
+class ConciliacaoService {
+
+
+	List<Guia> realizaConciliacao(List<GuiaHospital> guiasHospital, List<GuiaConvenio> guiasConvenio) {
+
+	}
+}
---
+++ src/test/groovy/br/com/zgsolucoes/glosaminn/service/conciliacao/ConciliacaoServiceTest.groovy
@@ -0,0 +1,14 @@
+package br.com.zgsolucoes.glosaminn.service.conciliacao
+
+import spock.lang.Specification
+
+class ConciliacaoServiceTest extends Specification {
+	void setup() {
+	}
+
+	void cleanup() {
+	}
+
+	def "RealizaConciliacao"() {
+	}
+}
