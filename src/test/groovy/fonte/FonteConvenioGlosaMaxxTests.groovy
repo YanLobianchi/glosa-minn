0b42fb627affeb0ca14c2e9e68de6d71fd30c8f2
Paulo Vitor <paulo.silva@zgsolucoes.com.br>
2019-12-20 17:22:11 -0300

    - Atualização dos testes Convenio

--- src/test/groovy/fonte/FonteConvenioGlosaMaxxTests.groovy
+++ src/test/groovy/fonte/FonteConvenioGlosaMaxxTests.groovy
@@ -18,11 +18,21 @@ class FonteConvenioGlosaMaxxTests extends Specification{
         then:
         guiasArquivo.size() == 2
 
+        guiasArquivo[0].dadosBeneficiario.nomeBeneficiario == 'CLERISVALDO SILVA SOUZA'
+        guiasArquivo[0].dadosBeneficiario.matricula == '106261029'
+        guiasArquivo[0].senha == '201600011781'
+        guiasArquivo[0].lote == '8550'
+        guiasArquivo[0].numeroGuiaPrestador == '173284000000'
         guiasArquivo[0].itens.size() == 29
         guiasArquivo[0].valorGlosa == 131.74
         guiasArquivo[0].valorApresentado == 1517.99
         guiasArquivo[0].valorPago == 1386.25
 
+        guiasArquivo[1].dadosBeneficiario.nomeBeneficiario == 'MARCOS ALVES DA SILVA'
+        guiasArquivo[1].dadosBeneficiario.matricula == '106208454'
+        guiasArquivo[1].senha == '201600024428'
+        guiasArquivo[1].lote == '8550'
+        guiasArquivo[1].numeroGuiaPrestador == '173175000000'
         guiasArquivo[1].itens.size() == 215
         guiasArquivo[1].valorGlosa == 1721.79
         guiasArquivo[1].valorApresentado == 8744.69
