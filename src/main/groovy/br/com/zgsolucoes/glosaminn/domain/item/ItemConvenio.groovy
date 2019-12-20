1a7d49b1b0251731851ca76f52b789acee99a4f0
igorDuarte <igor.duarte@zgsolucoes.com.br>
2019-12-20 11:36:17 -0300

    passando valorPago para dentro do itemConvenio e valorApresentado sendo valorTotal do item generico

--- src/main/groovy/br/com/zgsolucoes/glosaminn/domain/item/ItemConvenio.groovy
+++ src/main/groovy/br/com/zgsolucoes/glosaminn/domain/item/ItemConvenio.groovy
@@ -2,7 +2,7 @@ package br.com.zgsolucoes.glosaminn.domain.item
 
 class ItemConvenio extends Item{
 
-	BigDecimal valorApresentado
+	BigDecimal valorPago
 	BigDecimal valorGlosa
 	String motivoGlosa
 
