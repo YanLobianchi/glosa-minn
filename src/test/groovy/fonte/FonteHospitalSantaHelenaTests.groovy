40df91570e7334c9eaf40ba73a1f74494bb400e8
Paulo Cesar <paulobrito@zgsolucoes.com.br>
2019-12-20 17:05:07 -0300

    -> Refinando teste de aceitação

--- src/test/groovy/fonte/FonteHospitalSantaHelenaTests.groovy
+++ src/test/groovy/fonte/FonteHospitalSantaHelenaTests.groovy
@@ -2,6 +2,7 @@ package fonte
 
 import br.com.zgsolucoes.glosaminn.domain.fonte.FonteHospitalSantaHelena
 import guia.GuiaHospital
+import item.ItemHospital
 
 class FonteHospitalSantaHelenaTests extends GroovyTestCase {
 
@@ -29,6 +30,28 @@ class FonteHospitalSantaHelenaTests extends GroovyTestCase {
 		assertEquals "8551", primeiraGuia.lote
 		assertEquals "201600024210", primeiraGuia.senha
 		assertEquals 567.80, primeiraGuia.valorTotal.valorTotalGeral
+
+		assertEquals 11, primeiraGuia.itens.size()
+
+		ItemHospital itemExemplar = primeiraGuia.itens.first()
+
+		assertEquals "22", itemExemplar.codigoTabela
+		assertEquals "40805026", itemExemplar.codigoItem
+		assertEquals "RX - Torax - 2 incidencias", itemExemplar.descricaoItem
+		assertEquals 26.06, itemExemplar.valorUnitario
+		assertEquals 26.06, itemExemplar.valorTotal
+
+		GuiaHospital ultimaGuia = guiasList.last()
+
+		assertEquals "SIDNEY DE JESUS VALENTIM", ultimaGuia.dadosBeneficiario.nomeBeneficiario
+		assertEquals "105425613", ultimaGuia.dadosBeneficiario.matricula
+		assertEquals "N", ultimaGuia.dadosBeneficiario.atendimentoRn
+		assertEquals "201600001396", ultimaGuia.dadosBeneficiario.numero_guia_internacao
+		assertEquals "8551", ultimaGuia.lote
+		assertEquals "201600001396", ultimaGuia.senha
+		assertEquals 896.13, ultimaGuia.valorTotal.valorTotalGeral
+		assertEquals 276.60, ultimaGuia.valorTotal.valorProcedimentos
+		assertEquals 273.52, ultimaGuia.valorTotal.valorDiarias
 
 	}
 }
